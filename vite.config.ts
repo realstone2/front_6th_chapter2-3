e34061ce36b727717d2ef6bf15c846e72d4cc07a
진돌 <wlstjrghdud@naver.com>
2025-08-15 05:52:27 +0900

    feat: Vite 설정에 base 경로 추가

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/front_6th_chapter2-3/",
   build: {
     rollupOptions: {
       input: "./index.html",
