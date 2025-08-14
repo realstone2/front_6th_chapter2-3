30473591f5897358e0ddf2615f55e63563b87d7d
진돌 <wlstjrghdud@naver.com>
2025-08-15 03:40:34 +0900

    feat(deploy): GitHub Pages 배포를 위한 워크플로우 추가 및 Vite 설정 업데이트

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,14 @@ import react from "@vitejs/plugin-react"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/front_6th_chapter2-3/",
+  build: {
+    rollupOptions: {
+      input: "./index.html",
+    },
+    outDir: "dist",
+    copyPublicDir: true,
+  },
   server: {
     proxy: {
       "/api": {
