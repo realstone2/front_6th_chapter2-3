bc295b765a2f927eb9520d13299e817759e2ed8e
진돌 <wlstjrghdud@naver.com>
2025-08-15 00:14:22 +0900

    refactor(comment): update comment services

--- src/entities/comment/api/services.ts
+++ src/entities/comment/api/services.ts
@@ -1,7 +1,8 @@
 // #### 댓글 CRUD
 
 import { axiosInstance } from "../../../shared/lib/axios-instance"
+import { Comment, CommentsResponse } from "../model/types"
 
 export const getComments = (postId: number) => {
-  return axiosInstance.get(`/comments/post/${postId}`)
+  return axiosInstance.get<CommentsResponse>(`/comments/post/${postId}`)
 }
